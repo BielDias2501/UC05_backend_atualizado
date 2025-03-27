e8fc3a467a5707b9af94c4b5bcad5f20e1cee96f
BielDias2501 <gabrieldiascl@gmail.com>
2025-03-27 14:01:09 -0300

    add: adicionei o back end atualizado

---
+++ aula_2703/index.js
@@ -0,0 +1,20 @@
+// Importando com (ESM)
+const express = require('express')
+const dotenv = require('dotenv')
+const alunoRoutes = require('./src/modules/aluno/routes/index')
+dotenv.config();
+
+const port = process.env.PORTA;
+const app = express();
+
+//Aplicação use express com json
+app.use(express.json());
+
+// /aluno/matricula
+// /alunos
+
+app.use(alunoRoutes)
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
